ff2d2772c438e48f2d6a1b4a840a3391a9a8c428
Jiyeon <wisyeons@gmail.com>
2023-06-21 00:17:01 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,16 +2,15 @@ package com.example.demo.controller;
 import java.util.ArrayList; import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired; import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping; import org.springframework.web.bind.annotation.RequestMapping; import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.dto.ResponseDTO; import com.example.demo.service.TodoService;
 @RestController @RequestMapping("todo") public class TestController {
-	@Autowired
-	private TodoService service;
-	@GetMapping("/test")
-	public ResponseEntity<?>testTodo(){
-	String str = service.testService(); List<String> list =new ArrayList<>(); list.add(str);
-	ResponseDTO<String> response=
-			ResponseDTO.<String>builder().data(list).build();
-	return ResponseEntity.ok().body(response);
-	}
+@Autowired
+private TodoService service;
+@GetMapping("/test")
+public ResponseEntity<?>testTodo(){
+String str = service.testService(); List<String> list =new ArrayList<>(); list.add(str);
+ResponseDTO<String>
+response=ResponseDTO.<String>builder().data(list).build();
+return ResponseEntity.ok().body(response);
+}
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,20 @@
 package com.example.demo.service;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.TodoEntity;
+
+mport com.example.demo.persistence.TodoRepository;
 @Service
 public class TodoService {
-public String testService(){ return "Test Service";
-} }
+
+@Autowired
+private TodoRepository repository;
+public String testService(){ // Todo Entity 생성
+	TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+	// Todo Entity 저장
+	repository.save(entity);
+	// Todo Entity 검색
+	TodoEntity savedEntity = repository.findById(entity.getId()).get(); return savedEntity.getTitle();
+}
+	
+}
