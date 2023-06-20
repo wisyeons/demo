2c631ad429cdbbfdd30bda06f1257153a3023e6a
Jiyeon <wisyeons@gmail.com>
2023-06-21 00:22:31 +0900

    백엔드 개발 18. @NamedQuery, @Query 사용하기

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,18 +1,24 @@
 package com.example.demo.model;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue; import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import org.hibernate.annotations.GenericGenerator;
-import lombok.AllArgsConstructor; import lombok.Builder;
+import org.hibernate.annotations.GenericGenerator; import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Builder @NoArgsConstructor @AllArgsConstructor @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
-@Table(name ="Todo") public class TodoEntity {
-	@Id
-	@GeneratedValue(generator="system-uuid") // 자동으로 id 성성 @GenericGenerator(name="system-uuid",strategy="uuid")
-	private String id;
-	private String Userid;
-	private String title;
-	private boolean done;
+@Table(name ="Todo") 
+@Id
+@GeneratedValue(generator="system-uuid") // 자동으로 id 성성 @GenericGenerator(name="system-uuid",strategy="uuid")
+@GenericGenerator(name="system-uuid",strategy="uuid")
+private String id;
+private String userId; 
+private String title; 
+private boolean done;
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -7,13 +7,13 @@ import com.example.demo.persistence.TodoRepository;
 public class TodoService {
 	@Autowired
 	private TodoRepository repository;
-	public String testService(){
-		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-		// Todo Entity 저장 
-		repository.save(entity); 
-		// Todo Entity 검색 
-		TodoEntity
-		savedEntity=repository.findByUserId(entity.getUserId()).get(0); return savedEntity.getUserId();
-	}	
+	public String testService(){ 
+	// Todo Entity 생성
+	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+	// Todo Entity 저장
+	repository.save(entity);
+	// Todo Entity 검색
+	TodoEntity savedEntity =
+	repository.searchByUserId(entity.getUserId()).get(0); return savedEntity.getUserId();
 	}
+}
